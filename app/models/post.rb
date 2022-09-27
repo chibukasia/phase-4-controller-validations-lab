3249ab54191e1cfdb093049d6594cea711ef9d57
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-09-27 21:38:40 +0300

    commit tree 832b6e0f4e6ea2b7b1d37314278909ded8cca44d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}#, comparison: {equal_to: "Fiction"}
+    validates :category, inclusion: ["Fiction","Non-Fiction"]
 end
